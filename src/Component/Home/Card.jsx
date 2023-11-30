ce4ad287379d6d6d2bb38e3fb974d1bdc73de3b2
MuhammadSahossKhan <sahosskhan359@gmail.com>
2023-11-30 23:38:05 +0600

    fix aos  all issue

--- src/Component/Home/Card.jsx
+++ src/Component/Home/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({items}) => {
     // eslint-disable-next-line react/prop-types
     const {image,name}=items;
     return (
-        <div  data-aos="fade-up">
+        <div  >
             <div className="w-full max-w-xl overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
             <img className="object-cover w-full h-56" src={image} alt="avatar"/>
 
